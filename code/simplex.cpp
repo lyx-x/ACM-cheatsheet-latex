fdf4ceb822e31787cbd1d63062495f8fde4a2fdd
nsfxc <nnssffxxcc@gmail.com>
2015-11-19 15:45:07 +0100

    simplex

--- code/simplex.cpp
+++ code/simplex.cpp
@@ -38,7 +38,7 @@ struct LPSolver {
   LPSolver(const VVD &A, const VD &b, const VD &c) :
     m(b.size()), n(c.size()), N(n + 1), B(m), D(m + 2, VD(n + 2)) {
     for (int i = 0; i < m; i++) for (int j = 0; j < n; j++) D[i][j] = A[i][j];
-    for (int i = 0; i < m; i++) { B[i] = n + i; D[i][n] = -1; D[i][n + 1] = b[i]; }
+    for (int i = 0; i < m; i++) { B[i] = n + i; D[i][n] = -1; D[i][n + 1] = b[i];}
     for (int j = 0; j < n; j++) { N[j] = j; D[m][j] = -c[j]; }
     N[n] = -1; D[m + 1][n] = 1;
   }
@@ -59,14 +59,16 @@ struct LPSolver {
       int s = -1;
       for (int j = 0; j <= n; j++) {
         if (phase == 2 && N[j] == -1) continue;
-        if (s == -1 || D[x][j] < D[x][s] || D[x][j] == D[x][s] && N[j] < N[s]) s = j;
+        if (s == -1 || D[x][j] < D[x][s] || D[x][j] == D[x][s] && N[j] < N[s]) 
+		s = j;
       }
       if (D[x][s] > -EPS) return true;
       int r = -1;
       for (int i = 0; i < m; i++) {
         if (D[i][s] < EPS) continue;
         if (r == -1 || D[i][n + 1] / D[i][s] < D[r][n + 1] / D[r][s] ||
-          (D[i][n + 1] / D[i][s]) == (D[r][n + 1] / D[r][s]) && B[i] < B[r]) r = i;
+          (D[i][n + 1] / D[i][s]) == (D[r][n + 1] / D[r][s]) && B[i] < B[r]) 
+		r = i;
       }
       if (r == -1) return false;
       Pivot(r, s);
@@ -78,11 +80,13 @@ struct LPSolver {
     for (int i = 1; i < m; i++) if (D[i][n + 1] < D[r][n + 1]) r = i;
     if (D[r][n + 1] < -EPS) {
       Pivot(r, n);
-      if (!Simplex(1) || D[m + 1][n + 1] < -EPS) return -numeric_limits<DOUBLE>::infinity();
+      if (!Simplex(1) || D[m + 1][n + 1] < -EPS) 
+		return -numeric_limits<DOUBLE>::infinity();
       for (int i = 0; i < m; i++) if (B[i] == -1) {
         int s = -1;
         for (int j = 0; j <= n; j++)
-          if (s == -1 || D[i][j] < D[i][s] || D[i][j] == D[i][s] && N[j] < N[s]) s = j;
+          if (s == -1 || D[i][j] < D[i][s] || D[i][j] == D[i][s] && N[j] < N[s]) 
+		s = j;
         Pivot(i, s);
       }
     }
